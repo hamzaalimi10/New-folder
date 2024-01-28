a49ade6a52b8a2838595ce2f616400f5579da4a1
MusliHyseni <muslihyseni111@gmail.com>
2024-01-28 16:25:20 +0100

    Added Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,13 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container ">
-                <Link to="/" className='navbar-brand text-light'>Binge Station</Link>
+                <Link to="/" className='navbar-brand text-light'>
+                    <span className='m-3'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
+                            <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z"/>
+                        </svg>
+                    </span>
+                    Binge Station</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
